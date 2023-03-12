635048da6b15691b112f51a17614e7bfbd97e53a
Amantur <amantur710@gmail.com>
2023-03-12 20:56:24 +0600

    feat: add image bocks for explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,14 +1,20 @@
 "use client";
-
 import { motion } from "framer-motion";
 import { useState } from "react";
-
 import styles from "../styles";
 import { staggerContainer } from "../utils/motion";
 import { ExploreCard, TitleText, TypingText } from "../components";
 import {exploreWorlds} from "../constants"
+
+
+
+
 const Explore = () => {
-const [active, setActive] = useState(false)
+const [active, setActive] = useState("world-2")
+
+
+
+
   return (
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
