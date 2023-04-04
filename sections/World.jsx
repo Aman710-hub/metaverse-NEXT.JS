42e5f7dd14a855cd178e23183d9f2d4e8b8eb574
Amantur <amantur710@gmail.com>
2023-04-05 01:28:43 +0600

    feat: set up insight section

--- sections/World.jsx
+++ sections/World.jsx
@@ -6,7 +6,7 @@ import { TitleText, TypingText } from "../components";
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
-      variants={staggerContainer}
+      // variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{ once: "false", amount: 0.25 }}
